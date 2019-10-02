5e1d3b91e055b2aa194deb365de7b2cb224ae5fc
Krendelev <krendelev@gmail.com>
2019-10-02 07:45:46 +0300

    some modifications

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
 import os
-
+import json
 import requests
 from dotenv import load_dotenv
 from telegram import ParseMode
@@ -10,27 +10,32 @@ from settings import DVMN_URL, PROXY, VERDICT
 
 
 def get_task_status():
-    headers = {"Authorization": f"Token {os.environ['DVMN_TOKEN']}"}
-    timestamp = None
-    while True:
-        payload = {"timestamp": timestamp}
-        try:
-            response = requests.get(DVMN_URL, headers=headers, params=payload)
-        except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError):
-            continue
-        response.raise_for_status()
-        content = response.json()
-        timestamp = (
-            content.get("timestamp_to_request", None)
-            or content["last_attempt_timestamp"]
-        )
+    # headers = {"Authorization": f"Token {os.environ['DVMN_TOKEN']}"}
+    # timestamp = None
+    # while True:
+    #     payload = {"timestamp": timestamp}
+    #     try:
+    #         response = requests.get(DVMN_URL, headers=headers, params=payload)
+    #     except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError):
+    #         continue
+    #     response.raise_for_status()
+    #     content = response.json()
+    #     timestamp = (
+    #         content.get("timestamp_to_request", None)
+    #         or content["last_attempt_timestamp"]
+    #     )
+    with open("_workfiles/out.json") as fh:
+        content = json.load(fh)
         if content["status"] == "found":
-            for attempt in content["new_attempts"]:
-                message = f"""
-                    У вас проверили работу [{attempt['lesson_title']}](https://dvmn.org{attempt['lesson_url']})
-                    \n{VERDICT[attempt['is_negative']]}
-                """
-                yield message
+            yield content["new_attempts"]
+
+
+def compose_message(response):
+    message = f"""
+        У вас проверили работу [{response['lesson_title']}](https://dvmn.org{response['lesson_url']})
+        \n{VERDICT[response['is_negative']]}
+    """
+    return message
 
 
 def start(update, context):
@@ -38,7 +43,9 @@ def start(update, context):
         status = get_task_status()
     except requests.exceptions.HTTPError as error:
         update.message.reply_text(f"Ошибка при обращении к dvmn.org – {error}")
-    for msg in status:
+    for task in status:
+        print(status)
+        msg = compose_message(task)
         update.message.reply_text(msg, parse_mode=ParseMode.MARKDOWN)
 
 
