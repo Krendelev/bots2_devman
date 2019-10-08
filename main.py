5441ee5f0214979c64fce2a53cd45debce67b150
Krendelev <krendelev@gmail.com>
2019-10-08 09:45:38 +0300

    add handler

--- main.py
+++ main.py
@@ -1,62 +1,73 @@
 import logging
 import os
+import time
 
 import requests
 from dotenv import load_dotenv
 from telegram import ParseMode
-from telegram.ext import CommandHandler, Updater
+from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 
 from settings import DVMN_URL, VERDICT
 
 
-def get_task_status():
-    headers = {"Authorization": f"Token {os.environ['DVMN_TOKEN']}"}
-    timestamp = None
-    while True:
-        payload = {"timestamp": timestamp}
-        try:
-            response = requests.get(DVMN_URL, headers=headers, params=payload)
-        except (requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError):
-            continue
-        response.raise_for_status()
-        content = response.json()
-        timestamp = (
-            content.get("timestamp_to_request", None)
-            or content["last_attempt_timestamp"]
-        )
-        if content["status"] == "found":
-            yield from content["new_attempts"]
+class TGHandler(logging.Handler):
+    def __init__(self, token):
+        logging.Handler.__init__(self)
+        self.token = token
+
+        self.updater = Updater(self.token, use_context=True)
+        self.dispatcher = self.updater.dispatcher
+        self.bot = self.updater.bot
+        # self.dispatcher.add_handler(CommandHandler("start", emit))
 
 
-def compose_message(response):
-    message = f"""
-        У вас проверили работу [{response['lesson_title']}](https://dvmn.org{response['lesson_url']})
-        \n{VERDICT[response['is_negative']]}
-    """
-    return message
+def emit(self, record):
+    msg = self.format(record)
+    self.bot.send_message(chat_id=149258025, text=msg, disable_notification=True)
+    # self.bot.message.reply_text(msg, disable_notification=True)
 
 
 def start(update, context):
     try:
-        status = get_task_status()
-    except requests.exceptions.HTTPError as error:
-        update.message.reply_text(f"Ошибка при обращении к dvmn.org – {error}")
-    for task in status:
-        msg = compose_message(task)
-        update.message.reply_text(msg, parse_mode=ParseMode.MARKDOWN)
+        a = 5 / 0
+    except ZeroDivisionError as e:
+        logger.error(e)
+
+    c = abc()
+
+
+def abc():
+    b = [0]
+    return b[2]
+
+
+def error_callback(update, context):
+    logger.warning(f"{update.message.text} caused error {context.error}")
+
+
+def unknown(update, context):
+    update.message.reply_text(text="Sorry, I didn't understand that command.")
 
 
 if __name__ == "__main__":
-    logging.basicConfig(
-        format="%(asctime)s - %(levelname)s - %(message)s",
-        level=logging.INFO,
-        filename="bot.log",
-    )
     load_dotenv()
+
+    logging.basicConfig(
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        level=logging.INFO,
+        datefmt="%d-%b-%y %H:%M:%S",
+        # filename="bot.log",
+    )
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    TGHandler.emit = emit
+    logger.addHandler(TGHandler(os.environ["LOGGER_TOKEN"]))
 
     mybot = Updater(os.environ["TELEGRAM_TOKEN"], use_context=True)
 
     mybot.dispatcher.add_handler(CommandHandler("start", start))
+    mybot.dispatcher.add_handler(MessageHandler(Filters.command, unknown))
+    mybot.dispatcher.add_error_handler(error_callback)
 
     mybot.start_polling()
     mybot.idle()
