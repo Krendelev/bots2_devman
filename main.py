2222965c7af4fcfcf11f66bef33f5036fb8d4632
Krendelev <krendelev@gmail.com>
2019-10-09 12:09:54 +0300

    working

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
+import logging.config
 import os
-import time
 
 import requests
 from dotenv import load_dotenv
@@ -11,38 +11,35 @@ from settings import DVMN_URL, VERDICT
 
 
 class TGHandler(logging.Handler):
-    def __init__(self, token):
+    def __init__(self, token=None):
         logging.Handler.__init__(self)
         self.token = token
 
         self.updater = Updater(self.token, use_context=True)
         self.dispatcher = self.updater.dispatcher
         self.bot = self.updater.bot
-        # self.dispatcher.add_handler(CommandHandler("start", emit))
+        self.chat_id = os.environ["DEV_CHAT_ID"]
 
 
 def emit(self, record):
     msg = self.format(record)
-    self.bot.send_message(chat_id=149258025, text=msg, disable_notification=True)
-    # self.bot.message.reply_text(msg, disable_notification=True)
+    self.bot.send_message(chat_id=self.chat_id, text=msg, disable_notification=True)
 
 
 def start(update, context):
-    try:
-        a = 5 / 0
-    except ZeroDivisionError as e:
-        logger.error(e)
+    logging.info("Bot has started")
+    a = 5 / 0
 
     c = abc()
 
 
 def abc():
-    b = [0]
+    b = []
     return b[2]
 
 
 def error_callback(update, context):
-    logger.warning(f"{update.message.text} caused error {context.error}")
+    logging.warning(f"Bot encountered error: {context.error}")
 
 
 def unknown(update, context):
@@ -52,16 +49,12 @@ def unknown(update, context):
 if __name__ == "__main__":
     load_dotenv()
 
+    # TGHandler.emit = emit
+    # logging.config.fileConfig("logging.conf")
+    # logger = logging.getLogger("loggingBot")
     logging.basicConfig(
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        level=logging.INFO,
-        datefmt="%d-%b-%y %H:%M:%S",
-        # filename="bot.log",
+        format="%(asctime)s - %(levelname)s - %(message)s", filename="bot.log"
     )
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-    TGHandler.emit = emit
-    logger.addHandler(TGHandler(os.environ["LOGGER_TOKEN"]))
 
     mybot = Updater(os.environ["TELEGRAM_TOKEN"], use_context=True)
 
