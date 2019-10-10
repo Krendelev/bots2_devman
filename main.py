6e572d9130aa2ce7a645cd6c3c00a98c797d3370
Krendelev <krendelev@gmail.com>
2019-10-10 10:37:00 +0300

    use job_queue

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
 from settings import DVMN_URL, VERDICT
 
 
-class TGHandler(logging.Handler):
+class TgHandler(logging.Handler):
     def __init__(self, token=None):
         logging.Handler.__init__(self)
 
@@ -23,19 +23,48 @@ class TGHandler(logging.Handler):
 
 def emit(self, record):
     msg = self.format(record)
-    self.bot.send_message(chat_id=self.chat_id, text=msg, disable_notification=True)
+    self.bot.send_message(chat_id=self.chat_id, text=msg)
+
+
+def get_task_status(context):
+    headers = {"Authorization": f"Token {os.environ['DVMN_TOKEN']}"}
+    payload = {"timestamp": getattr(context, "timestamp", None)}
+
+    response = requests.get(DVMN_URL, headers=headers, params=payload)
+    response.raise_for_status()
+    content = response.json()
+    context.timestamp = (
+        content.get("timestamp_to_request", None) or content["last_attempt_timestamp"]
+    )
+    if content["status"] == "found":
+        yield from content["new_attempts"]
+
+
+def compose_message(response):
+    message = f"""
+        У вас проверили работу [{response['lesson_title']}](https://dvmn.org{response['lesson_url']})
+        \n{VERDICT[response['is_negative']]}
+    """
+    return message
+
+
+def send_status(context):
+    status = get_task_status(context)
+    for result in status:
+        msg = compose_message(result)
+        context.bot.send_message(
+            chat_id=context.job.context,
+            text=msg,
+            parse_mode=ParseMode.MARKDOWN,
+            disable_web_page_preview=True,
+        )
 
 
 def start(update, context):
     logger.info("Bot has been started")
-    a = 5 / 0
-
-    c = abc()
-
-
-def abc():
-    b = []
-    return b[2]
+    context.job_queue.run_repeating(
+        send_status, interval=90, first=0, context=update.message.chat_id
+    )
 
 
 def unknown(update, context):
@@ -43,15 +72,15 @@ def unknown(update, context):
 
 
 def error_callback(update, context):
-    logger.error(f"Bot encountered error: {context.error}", exc_info=True)
+    logger.warning(f"Bot encountered error: {context.error}")
 
 
 if __name__ == "__main__":
     load_dotenv()
 
-    TGHandler.emit = emit
+    TgHandler.emit = emit
     logging.config.fileConfig("logging.conf", disable_existing_loggers=False)
-    logger = logging.getLogger("loggingBot")
+    logger = logging.getLogger()
 
     mybot = Updater(os.environ["TELEGRAM_TOKEN"], use_context=True)
 
@@ -61,9 +90,3 @@ if __name__ == "__main__":
 
     mybot.start_polling()
     mybot.idle()
-
-    # logging.basicConfig(
-    #     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    #     level=logging.INFO,
-    #     # filename="bot.log",
-    # )
